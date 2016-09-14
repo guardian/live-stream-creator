88dafea6a09d2cae16d3abf3752db8138aae05b6
Akash A <akash1810@gmail.com>
2016-09-14 17:02:40 +0100

    jspm

---
+++ public/config.js
@@ -0,0 +1,14 @@
+System.config({
+  baseURL: "/assets",
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "traceur": "github:jmcriffey/bower-traceur@0.0.93",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93"
+  }
+});

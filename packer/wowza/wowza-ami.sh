1899acee2c3abecbb60c163eab3be6689722af8b
Akash A <akash1810@gmail.com>
2016-09-29 15:09:49 +0100

    update packer with some util scripts

--- packer/wowza/wowza-ami.sh
+++ packer/wowza/wowza-ami.sh
@@ -2,3 +2,14 @@
 
 rsync -a /home/ec2-user/config/ /usr/local/WowzaStreamingEngine/
 rm -rf /home/ec2-user/config
+
+service WowzaStreamingEngine restart
+
+# jq is needed for the scripts under bin/
+yum install jq -y
+
+# localhost:8087 needs to be available, wait for wowza to be ready before continuing
+sleep 2m
+
+/home/ec2-user/bin/enable-stream-targets
+/home/ec2-user/bin/enable-transcoder
